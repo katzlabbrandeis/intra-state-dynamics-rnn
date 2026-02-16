2780e607eec951646bcb1cf9bab9b46cb59945ce
Abuzar Mahmood <abuzarmahmood@gmail.com>
2026-02-16 17:20:56 -0500

    feat(utils): add poisson log likelihood and bits per spike calculation functions
    - Introduced `poisson_ll` function to compute Poisson log likelihood.
    - Added `calc_bits_per_spike` function to calculate bits per spike given a spike train and firing rate.
    - Imported `gammaln` from `scipy.special` for use in the Poisson log likelihood calculation.

--- src/abu/model_fitting/population_analysis/utils.py
+++ src/abu/model_fitting/population_analysis/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.special import gammaln
 
 class SpikeRasterIO:
     @staticmethod
@@ -26,3 +27,46 @@ class SpikeRasterIO:
         spike_train = np.zeros(original_array_shape, dtype=int)
         spike_train[tuple(spike_times)] = 1
         return spike_train
+
+
+def poisson_ll(lam, k):
+    """
+    Poisson log likelihood
+    # Note: This has been tested against scipy.stats.poisson.logpmf
+
+    Inputs:
+        lam: lambda parameter of poisson distribution
+        k: observed counts
+
+    Outputs:
+        ll: log likelihood
+
+    # From /media/bigdata/firing_space_plot/Mahmood_2025_directional_influence/poisson_glm/src/utils/utils.py
+    Ref: https://sherrytowers.com/2014/07/10/poisson-likelihood/
+    """
+    lam += 1e-10 # To ensure there is no log(0)
+    assert len(lam) == len(k), 'lam and k must be same length'
+    assert all(lam > 0), 'lam must be non-negative'
+    assert all(k >= 0), 'k must be non-negative'
+    return np.sum(k*np.log(lam) - lam - gammaln(k+1))
+
+def calc_bits_per_spike(spike_train, rate):
+    """
+    Calculate bits per spike for a given spike train and firing rate.
+    Args:
+        spike_train: numpy array of shape (trials, neurons, time_bins) with binary values indicating spikes
+        rate: firing rate in Hz (spikes per second)
+    Returns:
+        bits per spike
+
+    Refs:
+        - https://neuronaldynamics.epfl.ch/online/Ch10.S3.html
+        - https://www.biorxiv.org/content/10.1101/2025.02.07.637062v2.full
+    """
+    
+    mean_rate_ll = poisson_ll(rate, np.mean(spike_train)) 
+    given_rate_ll = poisson_ll(rate, spike_train)
+
+    bits_per_spike = (given_rate_ll - mean_rate_ll) / (np.log(2) * np.sum(spike_train)) 
+    
+    return bits_per_spike
