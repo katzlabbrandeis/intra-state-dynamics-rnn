dee1bb909d4d4412fa9181e2d14b7beebf0812be
Abuzar Mahmood <abuzarmahmood@gmail.com>
2026-04-30 17:31:58 -0400

    refactor(pop_latent_analysis): streamline latent extraction and plotting
    - Optimize latent extraction by processing latents for each session in a loop.
    - Remove commented-out code related to session trial counts.
    - Add plotting for all latent dimensions in a taste-by-taste grid.
    - Ensure plot saving functionality in the specified directory.
    - Import `product` from `itertools` for Cartesian product operations.

--- src/abu/pop_latent_analysis/extract_latents.py
+++ src/abu/pop_latent_analysis/extract_latents.py
@@ -5,6 +5,8 @@ from pprint import pprint as pp
 from matplotlib import pyplot as plt
 import xarray as xr
 import numpy as np
+# from tmux import tmux
+from itertools import product
 
 base_dir = '/media/bigdata/firing_space_plot/intra-state-dynamics-rnn'
 src_dir = os.path.join(base_dir, 'src')
@@ -106,39 +108,59 @@ latents = read_parquet_files_into_dict(
 # Testing
 ##############################
 
-# Check how many trials per taste for each session
-for session_key, session_latents in latents.items():
-    session_latents_df = session_latents.to_pandas()
-    taste_trial_counts = session_latents_df.groupby('taste')['trial'].nunique()
-    print(f"Session: {session_key}")
-    print(taste_trial_counts)
-    print("-" * 40)
-
 # Load latents for one session and plot n random trials 
 # session_key = 'AM11_4Tastes_191030_114043_repacked_raw_latent_vectors'
-session_key = 'AM11_4Tastes_191030_114043_rnn_latent_raw_output_unwarped'
-session_latents = latents[session_key]
+# session_key = 'AM11_4Tastes_191030_114043_rnn_latent_raw_output_unwarped'
 
-session_latents_df = session_latents.to_pandas()
-# Convert melt all columns with latent_dim
-latent_dim_cols = [col for col in session_latents_df.columns if col.startswith('latent_dim')]
-latent_dim_index = [int(col.split('_')[-1]) for col in latent_dim_cols]
-dim_name_to_ind_map = {col: int(col.split('_')[-1]) for col in latent_dim_cols}
-session_latents_melted = session_latents_df.melt(
-    id_vars=['taste', 'trial', 'time'], 
-    value_vars=latent_dim_cols, 
-    var_name='latent_dim', 
-    value_name='latent_value'
-)
-# Map latent_dim names to indices
-session_latents_melted['latent_dim'] = session_latents_melted['latent_dim'].map(dim_name_to_ind_map)
+plot_dir = '/media/bigdata/firing_space_plot/intra-state-dynamics-rnn/output/plots/population_analysis/latents_plots'
+if not os.path.exists(plot_dir):
+    os.makedirs(plot_dir)
 
-# Set multi-index to easily convert to xarray
-session_latents_melted.set_index(['taste', 'trial', 'latent_dim', 'time'], inplace=True)
+for session_key in latents.keys():
+    session_latents = latents[session_key]
 
-# Convert to xarray
-session_latents_xr = xr.Dataset.from_dataframe(session_latents_melted)
+    session_latents_df = session_latents.to_pandas()
+    # Convert melt all columns with latent_dim
+    latent_dim_cols = [col for col in session_latents_df.columns if col.startswith('latent_dim')]
+    latent_dim_index = [int(col.split('_')[-1]) for col in latent_dim_cols]
+    dim_name_to_ind_map = {col: int(col.split('_')[-1]) for col in latent_dim_cols}
+    session_latents_melted = session_latents_df.melt(
+        id_vars=['taste', 'trial', 'time'], 
+        value_vars=latent_dim_cols, 
+        var_name='latent_dim', 
+        value_name='latent_value'
+    )
+    # Map latent_dim names to indices
+    session_latents_melted['latent_dim'] = session_latents_melted['latent_dim'].map(dim_name_to_ind_map)
 
-# Convert to numpy array
-# Shape: (4, 30, 8, 119)
-session_latents_np = np.squeeze(session_latents_xr.to_array().values)
+    # Set multi-index to easily convert to xarray
+    session_latents_melted.set_index(['taste', 'trial', 'latent_dim', 'time'], inplace=True)
+
+    # Convert to xarray
+    session_latents_xr = xr.Dataset.from_dataframe(session_latents_melted)
+
+    # Convert to numpy array
+    # Shape: (4, 30, 8, 119)
+    session_latents_np = np.squeeze(session_latents_xr.to_array().values)
+
+    # # Plot all latents with taste x trial stacked
+    # stacked_latents = np.concatenate(session_latents_np, axis=0)  # Shape: (4*30, 8, 119)
+    # stacked_latents = np.swapaxes(stacked_latents, 0,1)  # Shape: (8, 4*30, 119) 
+
+
+    # Models were fit independently to each taste, so plot latents x taste grid
+    n_latent_dims = session_latents_np.shape[2]
+    n_tastes = session_latents_np.shape[0]
+    inds = list(product(range(n_latent_dims), range(n_tastes)))
+
+    fig , ax = plt.subplots(n_latent_dims, n_tastes, figsize=(4*n_tastes, 3*n_latent_dims), sharex=True, sharey=True)
+    for this_dim, this_taste in inds:
+        ax[this_dim, this_taste].imshow(
+                session_latents_np[this_taste, :, this_dim], 
+                aspect='auto', origin='lower', interpolation='none', cmap='jet')
+        ax[this_dim, this_taste].set_title(f'Latent Dimension {this_dim} - Taste {this_taste}')
+    plt.xlabel('Time')
+    plt.suptitle(f'Session: {session_key}\nAll Latent Dimensions (Taste x Trial stacked)')
+    plt.tight_layout()
+    fig.savefig(f"{plot_dir}/{session_key}_all_latent_dims_stacked.png")
+    plt.close(fig)
